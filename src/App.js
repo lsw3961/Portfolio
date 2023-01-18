ced8939ff7248ebcee217cec51487f7d7141f633
lsw3961 <lsw3961@g.rit.edu>
2023-01-18 00:42:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Logans Website
         </a>
       </header>
     </div>
