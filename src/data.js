a8f7cc3e9a187bcf59e6d700aaf7805c8f3c6892
Logan White (RIT Student) <lsw3961@rit.edu>
2023-01-18 16:02:24 -0500

    initial content added to website, need to record videos and add the rest but this is a start

---
+++ src/data.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: "Dreamcatcher",
+    subtitle: "Game Jam Submissoin",
+    description:
+      "A realtime light based survival game with pathtracking AI developed for an international game jam where the team and I ranked top 20",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Grapple",
+    subtitle: "Infinite Runner",
+    description:
+      "Grapple is a 3D parkour infinite runner with low gravity and real time physics applied through a grappling hook the player controls. Run fast and don't look back because one slip up and you're a goner.",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "Unity",
+  "Unreal Engine 4",
+  "HTML/CSS/Javascript",
+  "Skyrim Creation Kit",
+  "Maya",
+  "Github",
+  "React",
+  "Node",
+];
