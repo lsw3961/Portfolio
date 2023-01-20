4c346e86bbea225ad7ef8f4ffa15103480a4b092
lsw3961 <lsw3961@g.rit.edu>
2023-01-20 01:29:55 -0500

    added more projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,7 @@ export default function Projects() {
         <div className="flex flex-col w-full mb-20">
           <CodeIcon className="mx-auto inline-block w-10 mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
-            Games/Apps I've Built
+            Games/Apps/Levels I've Built
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
             Here is are few of my recent projects either from class or from my
--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
     subtitle: "Game Jam Submissoin",
     description:
       "A realtime light based survival game with pathtracking AI developed for an international game jam where the team and I ranked top 20",
-    image: "./project-1.gif",
+    image: "./dreamcatcher.gif",
     link: "https://lwhite322.itch.io/dreamcatcher",
   },
   {
@@ -12,27 +12,34 @@ export const projects = [
     subtitle: "Infinite Runner",
     description:
       "Grapple is a 3D parkour infinite runner with low gravity and real time physics applied through a grappling hook the player controls. Run fast and don't look back because one slip up and you're a goner.",
-    image: "./project-2.gif",
+    image: "./grapple.gif",
     link: "https://mhc4466.github.io/IGME320Project3/",
   },
+  {
+    title: "Portal 2 Level",
+    subtitle: "Momentum is key",
+    description: "A level that I made in the Portal 2 sweetheart editor that focuses on using momentum to throw the player to where they should go.",
+    image: "./portal.gif",
+    link: "https://youtu.be/Xn622uBCV_0"
+  }
 ];
 
-// export const testimonials = [
-//   {
-//     quote:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-//     image: "https://randomuser.me/api/portraits/men/1.jpg",
-//     name: "Jesse Hicks",
-//     company: "Zoozle",
-//   },
-//   {
-//     quote:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-//     image: "https://randomuser.me/api/portraits/men/94.jpg",
-//     name: "Ruben Alvarez",
-//     company: "Dooble.io",
-//   },
-// ];
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
 
 export const skills = [
   "Level / Enviromental Design",
