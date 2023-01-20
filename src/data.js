2fde5e6a41c4c51d90a29f02ab777ce73f14528a
lsw3961 <lsw3961@g.rit.edu>
2023-01-20 00:47:27 -0500

    tweaks

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export const projects = [
     description:
       "A realtime light based survival game with pathtracking AI developed for an international game jam where the team and I ranked top 20",
     image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://lwhite322.itch.io/dreamcatcher",
   },
   {
     title: "Grapple",
@@ -13,33 +13,37 @@ export const projects = [
     description:
       "Grapple is a 3D parkour infinite runner with low gravity and real time physics applied through a grappling hook the player controls. Run fast and don't look back because one slip up and you're a goner.",
     image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://mhc4466.github.io/IGME320Project3/",
   },
 ];
 
-export const testimonials = [
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
-  },
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io",
-  },
-];
+// export const testimonials = [
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/1.jpg",
+//     name: "Jesse Hicks",
+//     company: "Zoozle",
+//   },
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/94.jpg",
+//     name: "Ruben Alvarez",
+//     company: "Dooble.io",
+//   },
+// ];
 
 export const skills = [
+  "Level / Enviromental Design",
+  "Gameplay Design / Scripting",
+  "3D Modelling, Texturing",
+  "AI Encounter Design",
   "Unity",
   "Unreal Engine 4",
   "HTML/CSS/Javascript",
   "Skyrim Creation Kit",
-  "Maya",
+  "Autodesk Maya",
   "Github",
   "React",
   "Node",
