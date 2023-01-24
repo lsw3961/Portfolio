b6c6a1725d91aba278a3999ea3922f0a50904139
lsw3961 <lsw3961@g.rit.edu>
2023-01-23 22:03:55 -0500

    added some photos and did a few more deploys, still a work in progress
    going to keep adding but for now the website is mostly functional

--- src/data.js
+++ src/data.js
@@ -37,67 +37,67 @@ export const skills = [
   {
     content: "here is a level that I first did a few rough sketches of what I wanted it to be, then I improved those sketches by turning them into design documents focusing on the number of enemies, items, interactions, and paths. Finally I developed the level using the Far Cry 5 Arcade editor and then ran it though a few weeks of qa testing to determine what needed improvement.",
     title: "Level / Enviromental Design",
-    link: "https://github.com/lsw3961?tab=repositories",
-    image: "./placeholder.jpg",
+    link: "http://www.freepik.com",
+    image: "./comingSoon.jpg",
     index: 0
   },
   {
     content: "Below is a link to a quick webgame that I developed using Pixie, within it is an entire finite-state machine game focused on wave based survival.",
     title: "Gameplay Design / Scripting",
-    link: "",
-    image: "./placeholder.jpg",
+    link: "https://people.rit.edu/~lsw3961/235/project3/Game/",
+    image: "./pixieGame.png",
     index: 1
 
   },
   {
     content: "This below is a 3d modelling work that I did, in this scene I modeled an entire low poly castle including bushes, walls, gates, stalls, and barrels.",
     title: "3D Modelling, Texturing",
-    link: "",
-    image: "",
+    link: "http://www.freepik.com",
+    image: "./comingSoon.jpg",
     index: 2
   },
   {
     content: "Unity has been my primary engine for 5 years now and this is a photo of my first ever game built in Unity.",
     title: "Unity",
-    link: "",
-    image: "",
+    link: "https://lwhite322.itch.io/iroh",
+    image: "./firstGamePhoto.png",
     index: 3
   },
   {
     content: "Unreal Engine 4 is a newer tool to me but this project was my attempt as establishing the basics and getting a feel for C++ programming and blueprinting.",
     title: "Unreal Engine 4",
-    link: "",
-    image: "",
+    link: "https://lwhite322.itch.io/hourglass",
+    image: "./unrealProject.png",
     index: 4
   },
   {
     content: "Here is an interactive potion making website I made last semester. its a small but effecient and fluid interactive app.",
     title: "HTML / CSS / Javascript",
-    link: "",
-    image: "",
+    link: "https://people.rit.edu/lsw3961/ISTE340/Project1/ScaffoldingForP1.html",
+    image: "./PotionWebsiteScreenshot.png",
     index: 5
   },
   {
     content: "Below is a video link to a project that I was a part of, I designed the quests and NPCs",
     title: "Skyrim Creation Kit",
-    link: "",
-    image: "",
+    link: "https://www.youtube.com/watch?v=ncfjFhcpQAg&ab_channel=SamLambermont",
+    image: "./pandaMod.png",
     index: 6
 
   },
   {
     content: "Here is one of my favorites projects that I modeled in Autodesk Maya",
     title: "Autodesk Maya",
-    link: "",
-    image: "",
+    link: "http://www.freepik.com",
+    image: "./comingSoon.jpg",
     index: 7
 
   },
   {
     content: "Below is the link to my personal reposity, this entire website and most of my work is available on my public GitHub repositories",
     title: "Github",
-    link: "",
-    image: "",
+    link: "https://github.com/lsw3961?tab=repositories",
+    image: "./github.png",
     index: 8
   },
   {
@@ -118,9 +118,9 @@ export const skills = [
   {
     content: "The Icon at the top of the page as well as this design here are just some of my examples for the kind of work that I have done.",
     title: "Photoshop/Illustrator",
-    link: "",
-    image: "",
-    index: 10
+    link: "http://www.freepik.com",
+    image: "./comingSoon.jpg",
+    index: 11
   },
 ];
 
